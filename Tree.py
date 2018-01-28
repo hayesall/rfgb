cd6b8c974d9ace96725c34680e8ef90e092a76dc
kkroy36 <kkroy36@users.noreply.github.com>
2018-01-28 16:39:41 -0600

    Found rounding to 2 decimal places better.

--- Tree.py
+++ Tree.py
@@ -95,7 +95,7 @@ class node(object):
             elif curr.pos == "right":
                 clause += ""#"!"+curr.parent.test+","
             curr = curr.parent
-        if self.level == node.maxDepth or round(self.information,1) == 0:
+        if self.level == node.maxDepth or round(self.information,2) == 0:
             if clause[-1]!='-':
                 node.learnedDecisionTree.append(clause[:-1]+" "+str(Utils.getleafValue(self.examples)))
             else:
@@ -147,7 +147,7 @@ class node(object):
             self.right = node(None,bestFExamples,Utils.variance(bestFExamples),self.level+1,self,"right")
             if self.level+1 > node.depth:
                 node.depth = self.level+1
-        if self.test == "" or round(self.information,1) == 0: #if no examples append clause as is
+        if self.test == "" or round(self.information,2) == 0: #if no examples append clause as is
             if clause[-1]!='-':
                 node.learnedDecisionTree.append(clause[:-1])
             else:
