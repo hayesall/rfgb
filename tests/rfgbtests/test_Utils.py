b249e6ad3145d69ee3c2fc555cbfa9230c2fe069
batflyer <alexander@batflyer.net>
2018-04-05 11:54:52 -0500

    Adding metadata to test_Utils.py and making sure ordering is correct.

--- tests/rfgbtests/test_Utils.py
+++ tests/rfgbtests/test_Utils.py
@@ -1,20 +1,42 @@
+"""
+Copyright (C) 2017-2018 RFGB Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program (at the base of this repository). If not,
+see <http://www.gnu.org/licenses/>
+"""
+
 import sys
 import unittest
+
+# Metadata
+__author__     = "Alexander L. Hayes (@batflyer)"
+__copyright__  = "Copyright (C) 2017-2018 RFGB Contributors"
+__license__    = "GPL-v3"
+__maintainer__ = "Alexander L. Hayes (@batflyer)"
+__email__      = "alexander@batflyer.net"
+__status__     = "Prototype"
+
+"""
+Unit tests for Utils.py
+
+Tests should be ordered in the same manner as they are defined in Utils.py
+"""
 
 sys.path.append('./rfgb/')
 from Utils import Utils
 
 class UtilsTest(unittest.TestCase):
-
-    def test_sigmoid(self):
-        self.assertEqual(Utils.sigmoid(-5), 0.006692850924284856)
-        self.assertEqual(Utils.sigmoid(-0.001), 0.49975000002083336)
-        self.assertEqual(Utils.sigmoid(0), 0.5)
-        self.assertEqual(Utils.sigmoid(0.0001), 0.5000249999999792)
-        self.assertEqual(Utils.sigmoid(0.01), 0.5024999791668749)
-        self.assertEqual(Utils.sigmoid(0.5), 0.6224593312018546)
-        self.assertEqual(Utils.sigmoid(1), 0.7310585786300049)
-        self.assertEqual(Utils.sigmoid(5), 0.9933071490757152)
 
     def test_read_test_data_toy_cancer(self):
         """
@@ -66,6 +88,19 @@ class UtilsTest(unittest.TestCase):
             self.assertTrue(float(eVal) in list(sampleData.examples.values()))
 
         self.assertEqual(sampleData.examples, {'medv(id348)': 23.1, 'medv(id369)': 50.0, 'medv(id344)': 23.9, 'medv(id152)': 19.6, 'medv(id120)': 19.3, 'medv(id53)': 25.0, 'medv(id422)': 14.2, 'medv(id115)': 18.5, 'medv(id235)': 29.0, 'medv(id448)': 12.6, 'medv(id3)': 34.7, 'medv(id211)': 21.7, 'medv(id334)': 22.2, 'medv(id313)': 19.4, 'medv(id13)': 21.7, 'medv(id42)': 26.6, 'medv(id439)': 84.0, 'medv(id45)': 21.2, 'medv(id183)': 37.9, 'medv(id212)': 19.3, 'medv(id374)': 13.8, 'medv(id238)': 31.5, 'medv(id294)': 23.9, 'medv(id7)': 22.9, 'medv(id62)': 16.0, 'medv(id153)': 15.3, 'medv(id345)': 31.2, 'medv(id163)': 50.0, 'medv(id341)': 18.7, 'medv(id50)': 19.4, 'medv(id208)': 22.5, 'medv(id167)': 50.0, 'medv(id70)': 20.9, 'medv(id143)': 13.4, 'medv(id217)': 23.3, 'medv(id29)': 18.4, 'medv(id181)': 39.8, 'medv(id342)': 32.7, 'medv(id59)': 23.3, 'medv(id27)': 16.6, 'medv(id188)': 32.0, 'medv(id435)': 11.7, 'medv(id173)': 23.1, 'medv(id10)': 18.9})
+
+    def test_sigmoid(self):
+        """
+        tests: sigmoid function.
+        """
+        self.assertEqual(Utils.sigmoid(-5), 0.006692850924284856)
+        self.assertEqual(Utils.sigmoid(-0.001), 0.49975000002083336)
+        self.assertEqual(Utils.sigmoid(0), 0.5)
+        self.assertEqual(Utils.sigmoid(0.0001), 0.5000249999999792)
+        self.assertEqual(Utils.sigmoid(0.01), 0.5024999791668749)
+        self.assertEqual(Utils.sigmoid(0.5), 0.6224593312018546)
+        self.assertEqual(Utils.sigmoid(1), 0.7310585786300049)
+        self.assertEqual(Utils.sigmoid(5), 0.9933071490757152)
         
 if __name__ == '__main__':
     unittest.main()
