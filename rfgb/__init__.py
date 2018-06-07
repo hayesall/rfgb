920ff2ae4919f741fe3985e08078f42e9cea1cb4
batflyer <alexander@batflyer.net>
2018-06-07 14:22:21 -0500

    License iis no longer included in every docstring for each submodule of rfgb.

--- rfgb/__init__.py
+++ rfgb/__init__.py
@@ -1,3 +1,20 @@
+
+# Copyright (C) 2017-2018 RFGB Contributors
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program (at the base of this repository). If not,
+# see <http://www.gnu.org/licenses/>
+
 """
 rfgb.py
 =======
@@ -15,25 +32,6 @@ code, mostly consisting of high-level overviews and some usage examples.
 As this grows in complexity, documentation will likely be found in either
 the doc strings for individual functions (i.e. rfgb.__doc__), or online as
 https://starling.utdallas.edu/
-
-License
--------
-
-Copyright (C) 2017-2018 RFGB Contributors
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program (at the base of this repository). If not,
-see <http://www.gnu.org/licenses/>
 """
 
 from rfgb.utils import Utils
--- rfgb/boosting.py
+++ rfgb/boosting.py
@@ -1,5 +1,22 @@
+
+# Copyright (C) 2017-2018 RFGB Contributors
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program (at the base of this repository). If not,
+# see <http://www.gnu.org/licenses/>
+
 """
-rfgb.Boosting.py
+rfgb.boosting.py
 ================
 
 Provides
@@ -10,23 +27,6 @@ Provides
 Documentation
 -------------
 
-License
--------
-Copyright (C) 2017-2018 RFGB Contributors
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program (at the base of this repository). If not,
-see <http://www.gnu.org/licenses/>
 """
 
 from __future__ import division
