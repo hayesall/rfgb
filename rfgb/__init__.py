b689b1a0de668182f1aeab2b7a2c3b54a457cc8d
batflyer <alexander@batflyer.net>
2018-06-08 14:26:27 -0500

    Bumping version 0.2.0->0.2.1

--- rfgb/__init__.py
+++ rfgb/__init__.py
@@ -42,7 +42,7 @@ from rfgb.boosting import performInference
 __author__ = 'Kaushik Roy (@kkroy36)'
 __copyright__ = 'Copyright (c) 2017-2018 RFGB Contributors'
 __license__ = 'GNU General Public License v3.0 (GPLv3)'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __maintainer__ = 'Alexander L. Hayes'
 __email__ = 'alexander.hayes@utdallas.edu'
 __status__ = 'Prototype'
