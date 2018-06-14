9770835fcce5958cdb28d1a1e2e7de9cbd4d003f
batflyer <alexander@batflyer.net>
2018-06-14 11:16:48 -0500

    Changing imports from sys.path to relative imports. Changed GPL statements from docstrings to comments.

--- rfgb/tests/rfgbtests/test_Tree.py
+++ rfgb/tests/rfgbtests/test_Tree.py
@@ -1,29 +1,25 @@
-"""
-Copyright (C) 2017-2018 RFGB Contributors
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# Copyright (C) 2017-2018 RFGB Contributors
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program (at the base of this repository). If not,
-see <http://www.gnu.org/licenses/>
-"""
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program (at the base of this repository). If not,
+# see <http://www.gnu.org/licenses/>
 
 from __future__ import print_function
-
-import sys
+from __future__ import absolute_import
+from ...tree import node
 import unittest
 
-sys.path.append('./')
-
-from rfgb.tree import node
 
 class TreeTest(unittest.TestCase):
 
@@ -40,6 +36,3 @@ class TreeTest(unittest.TestCase):
 
         node.setMaxDepth(100)
         self.assertEqual(node.maxDepth, 100)
-
-if __name__ == '__main__':
-    unittest.main()
