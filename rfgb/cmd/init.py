e0cfabca5c774f7a40036cedb3a8ba9d24c5c8e3
batflyer <alexander@batflyer.net>
2018-07-06 14:22:11 -0400

    Adding init function based on the current definition in rfgb.__main__

---
+++ rfgb/cmd/init.py
@@ -0,0 +1,37 @@
+
+# Copyright (C) 2017-2018 RFGB Contributors
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program (at the base of this repository). If not,
+# see <http://www.gnu.org/licenses/>
+
+"""
+Initializes an empty .rfgb directory for storing models and metadata.
+"""
+
+import os
+import sys
+
+
+def init(quiet=False):
+
+    if not os.path.exists('.rfgb'):
+        os.makedirs('.rfgb')
+        os.makedirs('.rfgb/models/')
+
+        if not quiet:
+            print('Initialized empty rfgb repository at',
+                  os.path.abspath('.') + '/.rfgb/')
+    else:
+        print('.rfgb/ already exists.', file=sys.stderr)
+        exit(1)
